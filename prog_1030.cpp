1f5cb114c3862cd24822754eff4fdb8b51ad6124
rujroot <ruj9root@gmail.com>
2020-04-26 17:43:02 +0700

    Add code 1030 : Rice 8)

---
+++ prog_1030.cpp
@@ -0,0 +1,46 @@
+/*  1030 : Rice
+    ST : 11.40 26/4/63 ED : 17.37 26/4/63
+    O(n+Q) ???
+    Rujroot
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int n,P,Q,num,Buy;
+double Tmp;
+vector<pair<double,int>> Rice;
+double A(){
+    Tmp += Rice.back().first*Rice.back().second;
+    Buy -= Rice.back().second;
+    Rice.pop_back();
+    if(Rice.back().second < Buy){
+        return A();
+    }
+    return Tmp + Rice.back().first*Buy;
+}
+int main()
+{
+    scanf("%d",&n);
+    for(int i=0;i<n;++i){
+        scanf("%d %d",&P,&Q);
+        Rice.push_back({(double)P/(double)Q,Q});
+    }
+    sort(Rice.begin(),Rice.end(),greater<pair<double,int>>());
+    scanf("%d",&num);
+    for(int i=0;i<num;++i){
+        scanf("%d",&Buy);
+        if(Rice.back().second > Buy){
+            printf("%.3f\n",Rice.back().first*Buy);
+            Rice.back().second -= Buy;
+        }else if(Rice.back().second == Buy){
+            printf("%.3f\n",Rice.back().first*Buy);
+            Rice.pop_back();
+        }else{
+            Tmp = 0;
+            printf("%.3f\n",A());
+            Rice.back().second -= Buy;
+        }
+    }
+
+    return 0;
+}
+
