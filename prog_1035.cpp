55382734f16d4071f11c0352c9675e76af6057aa
rujroot <ruj9root@gmail.com>
2020-04-28 18:03:57 +0700

    Add code 1035 : PP :0

---
+++ prog_1035.cpp
@@ -0,0 +1,50 @@
+/*  1035 : Prime Palindrome
+    ST : 15.02 28/4/63 ED : 15.48 28/4/63
+    o(n*sqrt(n))
+    Rujroot
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+vector<int> Num;
+void makestring(int num){
+    if(num < 10){
+        Num.push_back(num % 10);
+    }else{
+        Num.push_back(num % 10);
+        makestring(num/10);
+    }
+
+}
+bool f(int n){
+    if(n < 2)  return 0;
+    int i = 2;
+    while(i*i <= n){
+        if(n%i==0)
+            return 0;
+        i = i + 1;
+    }
+    return 1;
+}
+int main()
+{
+    scanf("%d",&n);
+    for(int i=n;i<=2400000;++i){
+        if(f(i)){
+            bool have = true;
+            Num.clear();
+            makestring(i);
+            for(int i=0;i<Num.size()/2;++i){
+                if(Num[i] != Num[Num.size()-1-i]){
+                    have = false;
+                }
+            }
+            if(have){
+                printf("%d",i);
+                break;
+            }
+        }
+    }
+    return 0;
+}
+
