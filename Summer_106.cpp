bacbde94f2acdb6b6b44ac5fafbcae314943184a
rujroot <32482436+rujroot@users.noreply.github.com>
2020-12-24 23:20:49 +0700

    Update code summer 106 (bottom up)

--- Summer_106.cpp
+++ Summer_106.cpp
@@ -5,27 +5,39 @@ int DP[1010][1010];
 int N, K;
 vector<int> v;
 
-int Slove(int i, int j){
-    if(i == 0 and j == 0)
-        return 1;
-    if(i == 0)
-        return 1;
-    if(j < 0 or i < 0)
-        return 0;
-    if(DP[i][j])
-        return DP[i][j];
-    DP[i][j] += (Slove(i, j - 1) + Slove(i - v[j], j)) % 1000000007;
-;
-    return DP[i][j]; 
-}
-
 int main(){
     scanf("%d %d", &N, &K);
-    
+    v.push_back(0);
     for(int i = 0; i < N; ++i){
         int x; scanf("%d", &x);
         v.push_back(x);
     }
 
-    printf("%d", Slove(K, N - 1));
+    for(int i = 0; i <= K; ++i){
+        DP[i][0] = 0;
+    }
+    for(int i = 1; i <= N; ++i){
+        DP[0][i] = 1;
+    }
+    DP[0][0] = 1;
+
+    for(int i = 1; i <= K; ++i){
+        for(int j = 1; j < v.size(); ++j){
+            if(i - v[j] < 0 and j - 1 < 0){
+                DP[i][j] = 0;
+            }
+            else if(i - v[j] < 0){
+                DP[i][j] += DP[i][j-1] % 1000000007;
+
+            }
+            else if(j - 1 < 0){
+                DP[i][j] += DP[i - v[j]][j] % 1000000007;
+            }
+            else{
+                DP[i][j] += (DP[i][j - 1] + DP[i - v[j]][j]) % 1000000007;
+            }
+        }
+    }
+    printf("%d", DP[K][N]);
+
 }
