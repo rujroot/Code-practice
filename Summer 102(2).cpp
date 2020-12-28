62727e2a6f03d16854e90e7a410449a372cf464d
rujroot <32482436+rujroot@users.noreply.github.com>
2020-12-28 23:54:11 +0700

    Add code summer 102

---
+++ Summer 102(2).cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int cur[110][10100], prv[110][10100], a[110][110];
+unsigned long long int Ans[10100];
+bool Ask[10100];
+vector<int> v;
+
+int main(){
+    int n, m, t;
+    scanf("%d %d %d", &n, &m, &t);
+    for(int i = 1; i <= n; ++i){
+        for(int j = 1; j <= m; ++j){
+            int x; scanf("%d", &x);
+            a[i][j] = x;
+        }
+    }
+    for(int i = 0;i < t; ++i){
+        int x; scanf("%d", &x);
+        Ask[x] = true;
+        v.push_back(x);
+    }
+
+    cur[1][a[1][1]] = 1;
+    for(int j = 1; j <= m; ++j){
+        for(int k = 1; k < 10100; ++k){
+            if(j - 1 <= 0)
+                continue;
+            if(k - a[1][j] <= 0)
+                continue;
+            cur[j][k] = cur[j - 1][k - a[1][j]];
+        }  
+    }
+
+    for(int i = 2; i <= n; ++i){
+
+        for(int j = 1; j <= m; ++j){
+            for(int k = 1; k < 10100; ++k){
+                prv[j][k] = cur[j][k];
+                cur[j][k] = 0;
+            }
+        }
+
+        for(int j = 1; j <= m; ++j){
+            for(int k = 1; k < 10100; ++k){
+                if(k - a[i][j] <= 0)
+                    continue;
+                else if(j - 1 <= 0)
+                    cur[j][k] = prv[j][k - a[i][j]];
+                else
+                    cur[j][k] = cur[j - 1][k - a[i][j]] + prv[j][k - a[i][j]];
+
+                if(i == n and j == m and Ask[k]){
+                    //printf("-->%d %d\n", k, cur[j][k]);
+                    Ans[k] = cur[j][k]; 
+                } 
+
+            }
+        }
+        
+    }
+
+    for(int i = 0; i < v.size(); ++i){
+        printf("%lu\n", Ans[v[i]]);
+    }
+
+}
