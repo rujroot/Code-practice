83df30e01206a906105c4d259cb5f46c18847d28
rujroot <32482436+rujroot@users.noreply.github.com>
2020-12-29 23:03:32 +0700

    Update summer107.

--- Summer107.cpp
+++ Summer107.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int DP[70][70][70][70];
 bool visited[70][70][70][70];
 char ss1[70], ss2[70], ss3[70], ss4[70];
-string s1,s2,s3,s4;
+
 
 /*int Solve(int i, int j, int k, int l){
     if(i == -1 or j == -1 or k == -1 or l == -1)
@@ -19,23 +19,14 @@ string s1,s2,s3,s4;
 }*/
 
 int main(){
-    cin >> s1 >> s2 >> s3 >> s4;
-    int Si1 = s1.size(), Si2 = s2.size(), Si3 = s3.size(), Si4 = s4.size();
+    scanf("%s", ss1 + 1);
+    scanf("%s", ss2 + 1);
+    scanf("%s", ss3 + 1);
+    scanf("%s", ss4 + 1);
+    int Si1 = strlen(ss1 + 1), Si2 = strlen(ss2 + 1), Si3 = strlen(ss3 + 1), Si4 = strlen(ss4 + 1);
     
+    //cout << Si1 << Si2 << Si3 << Si4;
     //printf("%d",Solve(Si1 - 1, Si2 - 1, Si3 - 1, Si4 - 1));
-    for(int i = 0; i < Si1; ++i){
-        ss1[i + 1] = s1[i];
-    }
-    for(int i = 0; i < Si2; ++i){
-        ss2[i + 1] = s2[i];
-    }
-    for(int i = 0; i < Si3; ++i){
-        ss3[i + 1] = s3[i];
-    }
-    for(int i = 0; i < Si4; ++i){
-        ss4[i + 1] = s4[i];
-       
-    }
 
     for(int i = 1; i <= Si1; ++i){
         for(int j = 1; j <= Si2; ++j){
@@ -44,10 +35,11 @@ int main(){
                     if(ss1[i] == ss2[j] and ss2[j] == ss3[k] and ss3[k] == ss4[l])
                         DP[i][j][k][l] = 1 + DP[i - 1][j - 1][k - 1][l - 1];
                     else
-                        DP[i][j][k][l] = max(max(DP[i - 1][j][k][l], DP[i][j - 1][k][l]), max(DP[i][j][k - 1][l] ,DP[i][j][k][l - 1] ));
+                        DP[i][j][k][l] = max({DP[i - 1][j][k][l], DP[i][j - 1][k][l], DP[i][j][k - 1][l] , DP[i][j][k][l - 1] });
                 }
             }
         }
     }
     printf("%d", DP[Si1][Si2][Si3][Si4]);
+    return 0;
 }
