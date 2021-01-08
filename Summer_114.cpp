32f6161675f1580498efc1ec67f918e9981da84b
rujroot <32482436+rujroot@users.noreply.github.com>
2021-01-09 01:23:33 +0700

    Add Summer 114 (80/100)

---
+++ Summer_114.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool DP[110][50010];
+
+int main(){
+    int n, A[110];
+    scanf("%d", &n);
+
+    int sum = 0;
+    for(int i = 1; i <= n; ++i){
+        int x; scanf("%d", &x);
+        A[i] = x;
+        DP[i][0] = true;
+        sum += x;
+    }
+
+    if(sum % 2 != 0){
+        printf("No");
+        return 0;
+    }
+
+    for(int i = 1; i <= n; ++i){
+        for(int j = 1; j <= sum / 2; ++j){
+            DP[i][j] = DP[i - 1][j] or DP[i][j - A[i]];
+        }
+    }
+
+    if(DP[n][sum / 2])
+        printf("Yes");
+    else
+        printf("No");
+    
+    return 0;
+    
+}
