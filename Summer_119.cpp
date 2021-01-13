50e0453ed5dd12418b6a1a678bff6818c7768f2d
rujroot <32482436+rujroot@users.noreply.github.com>
2021-01-13 22:24:27 +0700

    Update new code

--- Summer_119.cpp
+++ Summer_119.cpp
@@ -9,14 +9,12 @@ bool Solve(){
 
     for(int i = 1; i <= L1; ++i){
         for(int j = 1; j <= L2; ++j){
-            if(i == 1 and j == 1 and S2[j] == '$')
-                DP[1][1] = true;
-            else if(S2[j] == '-' or S1[i] == S2[j])
+            if(S1[i] == S2[j] or S2[j] == '-')
                 DP[i][j] = DP[i - 1][j - 1];
-            else if(S2[j] != S1[i] and S2[j] != '$')
-                DP[i][j] = DP[i][j - 1] and DP[i - 1][j];
-            else
+            else if(S2[j] == '$')
                 DP[i][j] = DP[i - 1][j] or DP[i][j - 1];
+            else
+                DP[i][j] = false;
         }
     }
     return DP[L1][L2];
@@ -24,13 +22,13 @@ bool Solve(){
 
 
 int main(){
-    //printf("%d %d", 'a', 'z');
     scanf("%s", S1 + 1);
     scanf("%d", &n);
     L1 = strlen(S1 + 1);
 
-    
     DP[0][0] = true;
+   // DP[0][1] = true;
+   // DP[1][0] = true;
 
     for(int i = 1;i <= n; ++i){
         scanf("%s", S2 + 1);
@@ -41,17 +39,21 @@ int main(){
         else
             printf("fail\n");
 
-        /*for(int i = 1; i <= L1; ++i){
+        for(int i = 1; i <= L1; ++i){
              for(int j = 1; j <= L2; ++j){
                 printf("%d ", DP[i][j]);
              }printf("\n");
-        }*/
+        }
 
         for(int i = 0; i <= 2001; ++i){
              for(int j = 0; j <= 2001; ++j){
                 DP[i][j] = false;
              }
         }
+
         DP[0][0] = true;
+        // DP[0][1] = true;
+        // DP[1][0] = true;
+
     }
 }
