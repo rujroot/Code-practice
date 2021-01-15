999b65f6d1cd5aa75546de5cb0d0e9f9906739c9
rujroot <32482436+rujroot@users.noreply.github.com>
2021-01-15 23:25:51 +0700

    Add summer_121 (75/100)

---
+++ Summer_121.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool DP[110][50010];
+
+int main(){
+    int n, A[110], sum = 0;
+    scanf("%d", &n);
+    for(int i = 1; i <= n; ++i){
+        int x; scanf("%d", &x);
+        A[i] = x;
+        sum += x;
+        DP[i][0] = true;
+    }
+
+    for(int i = 1; i <= n; ++i){
+        for(int j = 1; j <= sum / 2; ++j){
+            DP[i][j] = DP[i - 1][j] or DP[i - 1][j - A[i]];
+        }
+    }
+
+    for(int i = sum / 2; i >= 0; ++i){
+        if(DP[n][i]){
+            printf("%d",abs(2 * i - sum));
+            return 0;
+        }
+    }
+}
