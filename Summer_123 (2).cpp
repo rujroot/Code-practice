bc4839f267e666bd7e9bf754d4e1883a97176bd8
rujroot <32482436+rujroot@users.noreply.github.com>
2021-01-18 23:04:23 +0700

    Add summer_123

---
+++ Summer_123 (2).cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool DP[110][110][2000];
+
+int main(){
+    int n, A[110], sum = 0, Qsum[110];
+    scanf("%d", &n);
+    for(int i = 1; i <= n; ++i){
+        int x; scanf("%d", &x);
+        A[i] = x;
+        sum += x;
+        Qsum[i] = sum;
+    }
+
+    if(sum % 3 != 0){
+        printf("noob");
+        return 0;
+    }
+    
+    for(int i = 0; i <= n ; ++i){
+        for(int j = 0; j <= n; ++j){
+            DP[i][j][0] = true;
+        }
+    }
+
+    DP[0][0][0] = true;
+
+    for(int i = 1; i <= n; ++i){
+         for(int j = i; j <= n; ++j){
+            for(int k = 1; k <= sum / 3; ++k){
+                if(k - A[j] >= 0)
+                    DP[i][j][k] = DP[i][j - 1][k] or DP[i][j - 1][k - A[j]];
+                else
+                    DP[i][j][k] = DP[i][j - 1][k];
+            }
+        }
+    }
+
+    for(int i = 1; i < n; ++i){
+        if(DP[1][i][sum / 3] and DP[i + 1][n][sum / 3]){
+            int Diff1 = Qsum[i] - (sum / 3), Diff2 = Qsum[n] - Qsum[i] - (sum / 3);
+            if(Diff1 + Diff2 == sum / 3){
+                printf("mission complete");
+                return 0;
+            }
+        }
+    }
+
+    printf("noob");
+    return 0;
+
+}
