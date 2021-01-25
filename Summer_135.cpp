52745fcb8d13c967f8a3794cd97c403aab182b62
rujroot <32482436+rujroot@users.noreply.github.com>
2021-01-26 00:17:16 +0700

    loop + bitwise 100/100

--- Summer_135.cpp
+++ Summer_135.cpp
@@ -1,30 +1,8 @@
-//ST : 22.12 25/1/64
 #include<bits/stdc++.h>
 using namespace std;
 
 double Ans = 9e18, A1[20], A2[20];
 int n;
-
-double Solve(int i, int Ch){
-    if(i > n)
-        return 9e18;
-    if(i == n){
-        double Sum1 = 0, Sum2 = 0, num1 = 0, num2 = 0;
-        for(int P = 0; P < n; ++P){
-            if(Ch & (1 << P)){//Shop 1
-                Sum1 += A1[P];
-                num1++;
-            }else{//Shop 2
-                Sum2 += A2[P];
-                num2++;
-            }
-        }
-        return (Sum1 - ((num1 / 100) * Sum1)) + (Sum2 - ((num2 / 100) * Sum2));
-    }
-    for(int l = 0; l < n; ++l){
-        Ans = min(Ans ,Solve(i + 1, Ch| (1 << l)));
-    }
-}
 
 int main(){
     scanf("%d", &n);
@@ -34,8 +12,22 @@ int main(){
     for(int i = 0; i < n; ++i){
         scanf("%lf", &A2[i]);
     }
-    Solve(0, 0);
-    printf("%.2f", Ans);
     
+    for(int i = 0; i <= (1 << n) - 1; ++i){
+        double Sum1 = 0, Sum2 = 0, num1 = 0, num2 = 0;
+        for(int P = 0; P < n; ++P){
+            if(i & (1 << P)){
+                Sum1 += A1[P];
+                num1++;
+            }else{
+                Sum2 += A2[P];
+                num2++;
+            }
+        }
+        Ans = min(Ans, (Sum1 - ((num1 / 100) * Sum1)) + (Sum2 - ((num2 / 100) * Sum2)));
+    }
+
+    printf("%.2f", Ans);
+    return 0;
 
 }
