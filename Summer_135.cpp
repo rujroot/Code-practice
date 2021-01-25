e743da55a17b16053ae5b557fe7cc307d502d110
rujroot <32482436+rujroot@users.noreply.github.com>
2021-01-26 00:06:41 +0700

    Summer_135 20/100

--- Summer_135.cpp
+++ Summer_135.cpp
@@ -1,18 +1,29 @@
 //ST : 22.12 25/1/64
-//ED : 23.51 25/1/64
 #include<bits/stdc++.h>
 using namespace std;
 
 double Ans = 9e18, A1[20], A2[20];
 int n;
 
-double Solve(int i, double Sum1, double Sum2, double num1, double num2){
-    if(i >= n){
-
-        return Ans = min(Ans, (Sum1 - ((num1 / 100) * Sum1)) + (Sum2 - ((num2 / 100) * Sum2)));
+double Solve(int i, int Ch){
+    if(i > n)
+        return 9e18;
+    if(i == n){
+        double Sum1 = 0, Sum2 = 0, num1 = 0, num2 = 0;
+        for(int P = 0; P < n; ++P){
+            if(Ch & (1 << P)){//Shop 1
+                Sum1 += A1[P];
+                num1++;
+            }else{//Shop 2
+                Sum2 += A2[P];
+                num2++;
+            }
+        }
+        return (Sum1 - ((num1 / 100) * Sum1)) + (Sum2 - ((num2 / 100) * Sum2));
     }
-    Solve(i + 1 , Sum1 + A1[i] ,Sum2, num1 + 1, num2);
-    Solve(i + 1, Sum1, Sum2 + A2[i], num1, num2 + 1);
+    for(int l = 0; l < n; ++l){
+        Ans = min(Ans ,Solve(i + 1, Ch| (1 << l)));
+    }
 }
 
 int main(){
@@ -23,7 +34,7 @@ int main(){
     for(int i = 0; i < n; ++i){
         scanf("%lf", &A2[i]);
     }
-    Solve(0, 0, 0, 0, 0);
+    Solve(0, 0);
     printf("%.2f", Ans);
     
 
