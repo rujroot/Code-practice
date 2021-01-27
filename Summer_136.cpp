c5a2637ef8ba03a0adaeeaa7b10c18e801feeead
rujroot <32482436+rujroot@users.noreply.github.com>
2021-01-28 01:55:16 +0700

    136 with greedy

--- Summer_136.cpp
+++ Summer_136.cpp
@@ -1,83 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-int n, m;
-double Ans = 9e18;
-
-double Buy(double A[5][5], double sum, bool Land[5][5], int count, int po1, int po2){
-    if(!Land[po1][po2]){
-        Land[po1][po2] = true;
-        sum += A[po1][po2];
-        count++;
-        A[po1 - 1][po2 - 1] += A[po1][po2] * 0.1;
-        A[po1 - 1][po2] += A[po1][po2] * 0.1;
-        A[po1 - 1][po2 + 1] += A[po1][po2] * 0.1;
-        A[po1][po2 - 1] += A[po1][po2] * 0.1;
-        A[po1][po2 + 1] += A[po1][po2] * 0.1;
-        A[po1 + 1][po2 - 1] += A[po1][po2] * 0.1;
-        A[po1 + 1][po2] += A[po1][po2] * 0.1;
-        A[po1 + 1][po2 + 1] += A[po1][po2] * 0.1;
-    }
-
-    double  SaveA[5][5];
-    bool SaveL[5][5];
-
+int main(){
+    int n, m;
+    double A[5][5];
+    scanf("%d %d", &n , &m);
     for(int i = 1; i <= n; ++i){
         for(int j = 1; j <= m; ++j){
-            SaveA[i][j] = A[i][j];
-            SaveL[i][j] = Land[i][j];
+            scanf("%lf", &A[i][j]);
         }
+    }
+
+    double Ans = 0;
+    for(int i = 1; i <= n * m; ++i){
+
+        int poi, poj;
+        double Min = 18e9;
+
+        for(int i = 1; i <= n ; ++i){
+            for(int j = 1; j <= m; ++j){
+                if(Min > A[i][j]){
+                    poi = i;
+                    poj = j;
+                    Min = A[i][j];
+                }
+            }
+        }
+
+        Ans += Min;
+        A[poi + 1][poj + 1] += A[poi][poj] * 0.1;
+        A[poi + 1][poj] += A[poi][poj] * 0.1;
+        A[poi + 1][poj - 1] += A[poi][poj] * 0.1;
+        A[poi][poj + 1] += A[poi][poj] * 0.1;
+        A[poi][poj - 1] += A[poi][poj] * 0.1;
+        A[poi - 1][poj + 1] += A[poi][poj] * 0.1;
+        A[poi - 1][poj] += A[poi][poj] * 0.1;
+        A[poi - 1][poj + 1] += A[poi][poj] * 0.1;
+        A[poi][poj] = 18e9;
+
     }
     
-    if(count == n*m){
-        Ans = min(Ans, sum);
-        return 0;
-    }
-
-    for(int i = 1; i <= n; ++i){
-        for(int j = 1; j <= m; ++j){
-            if(!Land[i][j])
-                Buy(A, sum, Land, count, i, j);
-
-            for(int a = 1; a <= n; ++a){
-                for(int b = 1; b <= m; ++b){
-                    A[a][b] = SaveA[a][b];
-                    Land[a][b] = SaveL[a][b];
-                }
-            }
-
-        }
-    }
-
-}
-
-int main(){
-    scanf("%d %d", &n, &m);
-
-    double Arr[5][5], Save[5][5];
-    bool LandP[5][5];
-
-    for(int i = 1; i <= n; ++i){
-        for(int j = 1; j <= m; ++j){
-            scanf("%lf", &Arr[i][j]);
-            LandP[i][j] = false;
-            Save[i][j] = Arr[i][j];
-        }
-    }
-
-    for(int i = 1; i <= n; ++i){
-        for(int j = 1; j <= m; ++j){
-            Buy(Arr, 0, LandP, 0, i, j);
-
-            for(int a = 1; a <= n; ++a){
-                for(int b = 1; b <= m; ++b){
-                    Arr[a][b] = Save[a][b];
-                    LandP[a][b] = false;
-                }
-            }
-
-        }
-    }
     printf("%.2f", Ans);
-    return 0;
+    
+
 }
