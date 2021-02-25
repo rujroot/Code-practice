19de09878df105ba11308c5978d181d594a55c70
rujroot <32482436+rujroot@users.noreply.github.com>
2021-02-25 23:12:45 +0700

    Add code

---
+++ CS_LookAndSay.cpp
@@ -0,0 +1,35 @@
+   
+#include <bits/stdc++.h>
+using namespace std;
+
+string s = "1";
+
+string lookAndSay(int N) {
+    N--;
+    while(N--){
+        char C = s[0]; 
+        int Q = 1;
+        string Copy;
+        s.push_back('-');
+        for(int i = 1; i < s.size(); ++i){
+            if(C != s[i]){
+                Copy.push_back(Q + 48);
+                Copy.push_back(C);
+                C = s[i];
+                Q = 1;
+                
+            }else{
+                Q++;
+            }
+            
+        }
+        s = Copy;
+    }
+    return s;
+}
+   
+int main() {
+    int N;
+    cin >> N;
+    cout << lookAndSay(N);
+}
